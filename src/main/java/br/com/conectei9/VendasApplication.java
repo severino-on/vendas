d59ca9584072170f6fe32ce1c586adfa499b86b7
Severino Silva <severino.silva@yaman.com.br>
2020-08-06 14:48:39 -0300

    Commit Incial

---
+++ src/main/java/br/com/conectei9/VendasApplication.java
@@ -0,0 +1,23 @@
+package br.com.conectei9;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+    @Autowired
+    private String applicationName;
+
+    @GetMapping("/hello")
+public String helloWorld(){
+    return applicationName;
+}
+
+public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
